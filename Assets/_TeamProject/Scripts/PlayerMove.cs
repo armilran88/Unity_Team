7a4c9843b729444955852409e5637af773e843d3
armilran88 <armilran88@gmail.com>
2025-11-19 17:31:01 +0900

    이번에는 안되겠지?
    안될거야 안~~~

--- Assets/_TeamProject/Scripts/PlayerMove.cs
+++ Assets/_TeamProject/Scripts/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour
         print("플레이어 무브");
         print("내꺼에서 디벨롭 제외하고 바로 메인으로 병합되나?");
         print("이룬 마스터로 바로 병합이 되어버리네?");
+        print("이번에는 브랜치 전략중 락시켜버리자");
     }
 
     // Update is called once per frame
