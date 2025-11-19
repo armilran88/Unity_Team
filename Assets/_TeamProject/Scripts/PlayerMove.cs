8449a7f86bf54c9faf16a56582488824042abebf
armilran88 <armilran88@gmail.com>
2025-11-19 17:51:38 +0900

    이것도 블락되라
    블락시켜보자

--- Assets/_TeamProject/Scripts/PlayerMove.cs
+++ Assets/_TeamProject/Scripts/PlayerMove.cs
@@ -9,6 +9,8 @@ public class PlayerMove : MonoBehaviour
         print("내꺼에서 디벨롭 제외하고 바로 메인으로 병합되나?");
         print("이룬 마스터로 바로 병합이 되어버리네?");
         print("브랜치전략에서 리드온리로 하니 안되는구먼");
+        print("이것도 안되겠지만 이건 되겠지?");
+
     }
 
     // Update is called once per frame
