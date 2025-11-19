aa493327fe7c6fa8e199519267e746d816e99ccc
armilran88 <armilran88@gmail.com>
2025-11-19 17:35:11 +0900

    이것도 안될텐데...
    이것도 안될텐데...

--- Assets/_TeamProject/Scripts/PlayerMove.cs
+++ Assets/_TeamProject/Scripts/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour
         print("플레이어 무브");
         print("내꺼에서 디벨롭 제외하고 바로 메인으로 병합되나?");
         print("이룬 마스터로 바로 병합이 되어버리네?");
+        print("브랜치전략에서 리드온리로 하니 안되는구먼");
     }
 
     // Update is called once per frame
