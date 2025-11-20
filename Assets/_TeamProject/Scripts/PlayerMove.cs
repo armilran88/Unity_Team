aaa0d1fbe20c610ec0581e0b5d27963322b27d4d
armilran88 <armilran88@gmail.com>
2025-11-20 09:52:34 +0900

    Update PlayerMove.cs

--- Assets/_TeamProject/Scripts/PlayerMove.cs
+++ Assets/_TeamProject/Scripts/PlayerMove.cs
@@ -6,9 +6,7 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         print("플레이어 무브");
-        print("내꺼에서 디벨롭 제외하고 바로 메인으로 병합되나?");
-        print("이룬 마스터로 바로 병합이 되어버리네?");
-        print("브랜치전략에서 리드온리로 하니 안되는구먼");
+        print("새로운 시작");
     }
 
     // Update is called once per frame
