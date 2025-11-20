d5401e5e4bbda2fc756539565bbbd93bb8771c3d
armilran88 <armilran88@gmail.com>
2025-11-20 14:33:03 +0900

    Update PlayerMove.cs

--- Assets/_TeamProject/Scripts/PlayerMove.cs
+++ Assets/_TeamProject/Scripts/PlayerMove.cs
@@ -6,9 +6,8 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         print("한글도 잘 나오지?");
-        //
-
         print("정말 잘 나오나?");
+        print("비쥬얼 스튜디오에서 병합을 어떻게 하지?");
     }
 
     // Update is called once per frame
